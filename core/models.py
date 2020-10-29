6be3a28363f0c0c9b0898c35ad7502719c99d844
Kirill Andreev <Hizenberg228@,ail.ru>
2020-10-29 16:04:41 +0300

    Add children for reviews

--- core/models.py
+++ core/models.py
@@ -131,7 +131,7 @@ class Review(models.Model):
     name = models.CharField("Имя", max_length=100)
     text = models.TextField("Сообщение", max_length=5000)
     parent = models.ForeignKey(
-        'self', verbose_name="Родитель", on_delete=models.CASCADE, null=True
+        'self', verbose_name="Родитель", on_delete=models.CASCADE, null=True, related_name='children'
     )
     movie = models.ForeignKey(Movie, verbose_name="Фильм", on_delete=models.CASCADE, related_name="reviews")
 
--- core/serializers.py
+++ core/serializers.py
@@ -10,6 +10,20 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ('title', 'tagline', 'category')
 
 
+class FilterReviewSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсвино children"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class ReviewCreateSerializer(serializers.ModelSerializer):
     """Дбавление комментария к фильму"""
 
@@ -20,10 +34,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Вывод комментариев"""
+    children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterReviewSerializer
         model = Review
-        fields = ('name', 'text', 'parent')
+        fields = ('name', 'text', 'children')
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
